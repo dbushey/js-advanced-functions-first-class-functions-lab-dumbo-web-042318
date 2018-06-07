dfd7ccbf2e962816a80c6a06d4a9d68d8dbdf1b2
dbushey <polendeuva@gmail.com>
2018-06-06 23:25:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the first two drivers in the array.
+const returnFirstTwoDrivers = function(array) {
+  return array.slice(0,2);
+};
+
+// Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the last two drivers in the array.
+const returnLastTwoDrivers = function(array) {
+  return array.slice(-2);
+};
+
+// This is an array containing two elements: the two functions that we previously defined (returnFirstTwoDrivers() and returnLastTwoDrivers()).
+// !!! elements of the array are const reference to the functions, NOT FUNCTIONS' CALLS
+const selectingDrivers = [
+  returnFirstTwoDrivers, returnLastTwoDrivers
+]
+
+// This is a higher-order function that takes in one argument, an integer, and returns a function that will multiply a fare for a ride accordingly
+const createFareMultiplier = function(int) { return function(value){ return int * value;
+  }
+};
+
+// Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareDoubler() function accepts a fare as its lone argument and doubles it.
+const fareDoubler = createFareMultiplier(2);
+
+// Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareTripler() function accepts a fare as its lone argument and triples it.
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers() — This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
+const selectDifferentDrivers = function(array, eitherFunction) {
+  return eitherFunction(array);
+};
